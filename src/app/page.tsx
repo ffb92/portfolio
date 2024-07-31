665233bc98608b285f5980c70d2194edce1f380b
ffb92 <finalflash.benni@gmail.com>
2024-07-31 16:37:44 +0200

    added framermotion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+"use client"
 import Link from 'next/link';
 import Particles from './components/particles';
+import { motion } from "framer-motion";
 
 const navigation = [
   { name: 'Projects', href: '/projects' },
   { name: 'Contact', href: '/contact' },
   { name: 'Skills', href: '/skills' },
   { name: 'Experience', href: '/experience' },
-
 ];
 
 export default function Home() {
@@ -25,15 +26,36 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Benjamin Westphal
-      </h1>
-
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
+        className="flex flex-col items-center"
+      >
+        <motion.div
+          className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        />
+        <motion.h1
+          className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text"
+          initial={{ opacity: 0, scale: 1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2.5, ease: "easeOut" }}
+        >
+          Benjamin Westphal
+        </motion.h1>
+        <motion.div
+          className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        />
+      </motion.div>
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-500">
           <p>Creative Web Developer focused on bringing unique ideas to life through code.</p>
         </h2>
       </div>
