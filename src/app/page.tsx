4415d105160611a83b21a955b8ad1edbfc665e41
ffb92 <finalflash.benni@gmail.com>
2024-08-01 16:25:02 +0200

    update sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const navigation = [
-  { name: 'Projects', href: '#section1' },
-  { name: 'Skills', href: '#section2' },
-  { name: 'Experience', href: '#section3' },
-  { name: 'Contact', href: '/contact' },
+  { name: 'About Me', href: '#about-me' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Experience', href: '#experience' },
+  { name: 'Contact', href: '#contact' },
 ];
 
 export default function Home() {
@@ -33,64 +34,85 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"></div>
-      <main className="">
-        <div className="flex flex-col items-center justify-center bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1.5, ease: 'easeOut' }}
-            className="flex flex-col items-center"
-          >
+      <div className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0">
+        <main className="bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+          <div className="flex flex-col items-center justify-center  min-h-screen">
             <motion.div
-              className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
-              initial={{ opacity: 1 }}
-              animate={{ opacity: 0 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
               transition={{ duration: 1.5, ease: 'easeOut' }}
-            />
-            <motion.h1
-              className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text"
-              initial={{ opacity: 0, scale: 1 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 2.5, ease: 'easeOut' }}
+              className="flex flex-col items-center"
             >
-              Benjamin Westphal
-            </motion.h1>
-            <motion.div
-              className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
-              initial={{ opacity: 1 }}
-              animate={{ opacity: 0 }}
-              transition={{ duration: 1.5, ease: 'easeOut' }}
-            />
-          </motion.div>
-          <div className="my-16 text-center animate-fade-in">
-            <h2 className="text-sm text-zinc-500">
-              <p>Creative Web Developer focused on bringing unique ideas to life through code.</p>
-            </h2>
+              <motion.div
+                className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 1.5, ease: 'easeOut' }}
+              />
+              <motion.h1
+                className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text"
+                initial={{ opacity: 0, scale: 1 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 2.5, ease: 'easeOut' }}
+              >
+                Benjamin Westphal
+              </motion.h1>
+              <motion.div
+                className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 1.5, ease: 'easeOut' }}
+              />
+            </motion.div>
+
+            <div className="my-16 text-center animate-fade-in">
+              <h2 className="text-sm text-zinc-500">
+                <p>Creative Web Developer focused on bringing unique ideas to life through code.</p>
+              </h2>
+            </div>
           </div>
-          <section id="section1" className="">
-            <h2 className="text-9xl">Section 1</h2>
+          <div className="hidden w-screen h-3 md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/50" />
+          <section id="about-me">
+            <h2 className="text-9xl">Hello 👋</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quia fugit
               dolorum et minima eum adipisci, eveniet commodi enim nisi.
             </p>
           </section>
-          <section id="section2" className="">
-            <h2 className="text-9xl">Section 2</h2>
+          <div className="hidden w-screen h-3 md:block bg-gradient-to-r from-zinc-300/50 via-zinc-300/50 to-zinc-300/0" />
+          <section id="skills" className="">
+            <h2 className="text-9xl">Skills</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quia fugit
               dolorum et minima eum adipisci, eveniet commodi enim nisi.
             </p>
           </section>
-          <section id="section3" className="">
-            <h2 className="text-9xl">Section 3</h2>
+          <div className="hidden w-screen h-3 md:block bg-gradient-to-r from-zinc-300/50 via-zinc-300/50 to-zinc-300/0" />
+          <section id="projects" className="">
+            <h2 className="text-9xl">Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quia fugit
+              dolorum et minima eum adipisci, eveniet commodi enim nisi.
+            </p>
+          </section>
+          <div className="hidden w-screen h-3 md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/50" />
+          <section id="experience" className="">
+            <h2 className="text-9xl">Experience</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laborum ex veniam
               quis doloremque ea distinctio in possimus fuga blanditiis.
             </p>
           </section>
-        </div>
-      </main>
+          <div className="hidden w-screen h-3 md:block bg-gradient-to-r from-zinc-300/50 via-zinc-300/50 to-zinc-300/0" />
+          <section id="contact" className="">
+            <h2 className="text-9xl">Contact</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laborum ex veniam
+              quis doloremque ea distinctio in possimus fuga blanditiis.
+            </p>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
